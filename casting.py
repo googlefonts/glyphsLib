5e6be3cfc2dacd941e0e0d900db0d75c28c86dd2
James Godfrey-Kittle <jamesgk@google.com>
2015-06-19 16:49:39 -0700

    Initial commit.

---
+++ casting.py
@@ -0,0 +1,193 @@
+__all__ = [
+    'cast_data'
+]
+
+
+import json
+import re
+
+
+def cast_data(data, types=None):
+    """Cast the attributes of parsed glyphs file content."""
+
+    print_dbg = False
+    if types is None:
+        print_dbg = True
+        types = get_type_structure()
+
+    new_data = {}
+    for key, cur_type in types.items():
+        if key not in data:
+            continue
+        if type(cur_type) == dict:
+            new_data[key] = []
+            for cur_data in data[key]:
+                new_data[key].append(cast_data(cur_data, dict(cur_type)))
+            data[key] = [dict(cur_data) for cur_data in data[key] if cur_data]
+            if not data[key]:
+                del data[key]
+        else:
+            new_data[key] = cur_type(data[key])
+            del data[key]
+
+    if print_dbg:
+        print 'not casted in data:', json.dumps(data, indent=2, sort_keys=True)
+    return new_data
+
+
+# should see: DisplayStrings not found, fontMaster/weight not casted
+def get_type_structure():
+    """Generate and return the highest-level type hierarchy for glyphs data."""
+
+    return {
+        'DisplayStrings': list,
+        'classes': {
+            'automatic': bool,
+            'code': str,
+            'name': str
+        },
+        'copyright': str,
+        'customParameters': {
+            'name': str,
+            'value': default
+        },
+        'date': str,  #TODO(jamesgk) need custom method here?
+        'designer': str,
+        'designerURL': str,
+        'familyName': str,
+        'featurePrefixes': {
+            'code': str,
+            'name': str
+        },
+        'features': {
+            'automatic': bool,
+            'code': str,
+            'name': str
+        },
+        'fontMaster': {
+            'alignmentZones': pointlist,
+            'ascender': int,
+            'capHeight': int,
+            'customParameters': {
+                'name': str,
+                'value': default
+            },
+            'descender': int,
+            'horizontalStems': intlist,
+            'id': str,
+            'userData': dict,
+            'verticalStems': intlist,
+            'weightValue': int,
+            'xHeight': int
+        },
+        'glyphs': {
+            'glyphname': str,
+            'lastChange': str,  #TODO(jamesgk) also a date
+            'layers': {
+                'anchors': {
+                    'name': str,
+                    'position': point
+                },
+                'components': {
+                    'anchor': str,
+                    'name': str,
+                    'transform': transform
+                },
+                'associatedMasterId': str,
+                'background': dict,  #TODO has same children as layer
+                'layerId': str,
+                'leftMetricsKey': str,
+                'rightMetricsKey': str,
+                'name': str,
+                'paths': {
+                    'closed': bool,
+                    'nodes': nodelist
+                },
+                'width': float
+            },
+            'leftKerningGroup': str,
+            'leftMetricsKey': str,
+            'rightKerningGroup': str,
+            'rightMetricsKey': str,
+            'unicode': str  #TODO(jamesgk) could be parsed as hex value
+        },
+        'instances': {
+            'customParameters': {
+                'name': str,
+                'value': default
+            }
+        },
+        'kerning': kerning,
+        'manufacturer': str,
+        'manufacturerURL': str,
+        'unitsPerEm': int,
+        'userData': dict,
+        'versionMajor': int,
+        'versionMinor': int
+    }
+
+
+def default(value):
+    """Just return the value (i.e. don't cast it to anything)."""
+    return value
+
+
+def vector(string, dimension):
+    """Parse a vector from a string with format {X, Y, Z, ...}."""
+
+    rx = '{%s}' % ', '.join(['([-.\d]+)'] * dimension)
+    return [float(i) for i in re.match(rx, string).groups()]
+
+
+def point(string):
+    return vector(string, 2)
+
+
+def transform(string):
+    return vector(string, 6)
+
+
+def node(string):
+    """Cast a node from a string with format X Y TYPE [SMOOTH]."""
+
+    rx = '([-.\d]+) ([-.\d]+) (LINE|CURVE|OFFCURVE)(?: (SMOOTH))?'
+    m = re.match(rx, string).groups()
+    return [float(m[0]), float(m[1]), m[2], m[3]]
+
+
+def castlist(strlist, cast):
+    """Cast a list of strings."""
+    return [cast(string) for string in strlist]
+
+
+def intlist(strlist):
+    return castlist(strlist, int)
+
+
+def pointlist(strlist):
+    return castlist(strlist, point)
+
+
+def nodelist(strlist):
+    return castlist(strlist, node)
+
+
+def kerning(kerning_data):
+    """Cast the values in kerning data to ints."""
+
+    new_data = {}
+    for master_id, master_map in kerning_data.items():
+        new_data[master_id] = {}
+        for left_glyph, glyph_map in master_map.items():
+            new_data[master_id][left_glyph] = {}
+            for right_glyph, value in glyph_map.items():
+                new_data[master_id][left_glyph][right_glyph] = int(value)
+    return new_data
+
+
+def version_minor(string):
+    """Ensure that the minor version number is between 0 and 999."""
+
+    num = int(string)
+    assert num >= 0 and num <= 999
+    return num
---
+++ glyphslib.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+
+__all__ = [
+    "load", "loads",
+]
+
+import binascii
+import codecs
+from io import BytesIO
+
+from casting import cast_data
+
+class Parser:
+	def __init__(self, dict_type):
+		self.stack = []
+		self.current_key = None
+		self.root = None
+		self._dict_type = dict_type
+		self.lineno = 1
+
+	def parse(self, st):
+		i = 0
+		c = st[i];i+=1
+		try:
+			while True:
+				if c == b'\n':
+					self.lineno+=1
+					c = st[i];i+=1
+					continue
+				if c in ' \r\t':
+					c = st[i];i+=1
+					continue
+				elif c in b'{=;}(,)<>':
+					self.handlers[c](self, c)
+					c = st[i];i+=1
+				elif c == b'"':
+					start = i
+					while True:
+						c = st[i];i+=1
+						if c in '"\\':
+							break
+					if c == b'"':
+						self.add_object(st[start:i-1])
+					else:
+						s = bytearray(st[start:i-1])
+						i-=1
+						while True:
+							c = st[i];i+=1
+							if c == b'"':
+								break
+							if c == b'\\':
+								c = st[i];i+=1
+								s.append(c)
+								continue
+							s.append(c)
+						self.add_object(bytes(s))
+					c = st[i];i+=1
+				elif b'a'<=c<=b'z' or b'A'<=c<=b'Z' or b'0'<=c<=b'9' or c in b'._':
+					start = i - 1
+					while True:
+						c = st[i];i+=1
+						if b'a'<=c<=b'z' or b'A'<=c<=b'Z' or b'0'<=c<=b'9' or c in b'._':
+							pass
+						else:
+							break
+					self.add_object(st[start:i-1])
+				else:
+					assert 0, "Unexpected character '%s'" % c
+		except IndexError:
+			pass
+		assert not self.stack
+		return self.root
+
+	def add_object(self, value):
+		if not self.stack:
+			# this is the root object
+			self.root = value
+		elif isinstance(self.stack[-1], type([])):
+			self.stack[-1].append(value)
+		elif isinstance(self.stack[-1], self._dict_type):
+			if self.current_key is None:
+				self.current_key = value
+			else:
+				self.stack[-1][self.current_key] = value
+				self.current_key = None
+		else:
+			assert 0
+
+	# element handlers
+
+	def unexpected_token(self, tok):
+		return	ValueError("Unexpected character '%s' at line %d" %
+				   (tok, self.lineno))
+
+	def begin_dict(self, _):
+		d = self._dict_type()
+		self.add_object(d)
+		self.stack.append(d)
+
+	def end_dict(self, _):
+		if self.current_key:
+			raise ValueError("missing value for key '%s' at line %d; missing semi-colon?" %
+							 (self.current_key,0))
+		self.stack.pop()
+
+	def dict_key(self, tok):
+		if not self.current_key or not isinstance(self.stack[-1], self._dict_type):
+			raise self.unexpected_token(tok)
+
+	def dict_value(self, tok):
+		if self.current_key or not isinstance(self.stack[-1], self._dict_type):
+			raise self.unexpected_token(tok)
+
+	def begin_array(self, _):
+		a = []
+		self.add_object(a)
+		self.stack.append(a)
+
+	def array_item(self, tok):
+		if not isinstance(self.stack[-1], type([])):
+			raise self.unexpected_token(tok)
+
+	def end_array(self, _):
+		self.stack.pop()
+
+	handlers = {
+		'{': begin_dict,
+		'=': dict_key,
+		';': dict_value,
+		'}': end_dict,
+		'(': begin_array,
+		',': array_item,
+		')': end_array,
+	}
+
+
+def load(fp, dict_type=dict):
+	"""Read a .glyphs file. 'fp' should be (readable) file object.
+	Return the unpacked root object (which usually is a dictionary).
+	"""
+	return loads(fp.read(), dict_type=dict_type)
+
+
+def loads(value, dict_type=dict):
+	"""Read a .glyphs file from a bytes object.
+	Return the unpacked root object (which usually is a dictionary).
+	"""
+	p = Parser(dict_type=dict_type)
+	return p.parse(value)
+
+import sys
+f = open(sys.argv[1], 'rb')
+import collections
+glyphs = load(f, dict_type=collections.OrderedDict)
+
+#from pprint import pprint
+#pprint(glyphs)
+
+glyphs = cast_data(glyphs)
+
+import json
+print(json.dumps(glyphs, indent=2, sort_keys=True).decode())
