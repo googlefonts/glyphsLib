7fa60d24f4992dd450974c98a59857f86750fe43
James Godfrey-Kittle <jamesgk@google.com>
2015-07-14 11:06:33 -0700

    Generate TTFs with glyphslib.py.

--- glyphslib.py
+++ glyphslib.py
@@ -7,8 +7,11 @@ __all__ = [
 import json
 import sys
 
+from fontbuild.convertCurves import glyphCurvesToQuadratic
+from fontbuild.outlineTTF import OutlineTTFCompiler
+
 from parser import Parser
-from casting import cast_data
+from casting import cast_data, cast_noto_data
 from torf import to_robofab
 
 
@@ -24,15 +27,32 @@ def loads(value, dict_type=dict):
 	Return the unpacked root object (which usually is a dictionary).
 	"""
 	p = Parser(dict_type=dict_type)
-	return cast_data(p.parse(value))
+	print '>>> Parsing .glyphs file'
+	data = p.parse(value)
+	print '>>> Casting parsed values'
+	data = cast_data(data)
+	cast_noto_data(data)
+	return data
+	#return cast_data(data, print_dbg=True)
 
 
 def load_to_rfonts(filename):
-    return to_robofab(load(open(filename, 'rb')))
+    """Load an unpacked .glyphs object to a RoboFab RFont."""
+    data = load(open(filename, 'rb'))
+    print '>>> Loading to RFonts'
+    return to_robofab(data)
 
 
 def main(argv):
+    #print json.dumps(load(open(sys.argv[1], 'rb')), indent=2, sort_keys=True)
     rfonts = load_to_rfonts(sys.argv[1])
+    for font in rfonts:
+        ofile = font.info.postscriptFullName + '.ttf'
+        print '>>> Compiling %s' % ofile
+        for glyph in font:
+            glyphCurvesToQuadratic(glyph)
+        compiler = OutlineTTFCompiler(font, ofile)
+        compiler.compile()
 
 
 if __name__ == '__main__':
