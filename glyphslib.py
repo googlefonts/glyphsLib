47dead1c0af7798f8dd3b78bb4d41fd85abff1c3
James Godfrey-Kittle <jamesgk@google.com>
2015-07-23 18:33:58 -0700

    Add instance generation to glyphslib.py.
    This may not be the final place for this, but for now it's an
    experiment in using MutatorMath to generate instances. Almost
    working except for some naming issues.

--- glyphslib.py
+++ glyphslib.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 __all__ = [
-    "load_to_rfonts", "load", "loads",
+    "load_to_rfonts", "build_instances", "load", "loads",
 ]
 
 import json
+import os
 import sys
 
 from fontbuild.convertCurves import glyphCurvesToQuadratic
@@ -12,7 +13,8 @@ from fontbuild.outlineTTF import OutlineTTFCompiler
 
 from parser import Parser
 from casting import cast_data, cast_noto_data
-from torf import to_robofab
+from torf import to_robofab, clear_data, build_style_name
+from torf import build_postscript_name, build_style_map_style
 
 
 def load(fp, dict_type=dict):
@@ -39,13 +41,13 @@ def load_to_rfonts(filename):
     """Load an unpacked .glyphs object to a RoboFab RFont."""
     data = load(open(filename, 'rb'))
     print '>>> Loading to RFonts'
-    return to_robofab(data)
+    return to_robofab(data, include_instances=True)
     #return to_robofab(data, debug=True)
 
 
 def save_ufo(font):
     """Save an RFont as a UFO."""
-    ofile = font.info.postscriptFullName + '.ufo'
+    ofile = os.path.join('master_ufo', font.info.postscriptFullName + '.ufo')
     print '>>> Compiling %s' % ofile
     font.save(ofile)
 
@@ -60,9 +62,64 @@ def save_ttf(font):
     compiler.compile()
 
 
+def build_instances(rfonts, instances):
+    """Create MutatorMath designspace and generate instances."""
+
+    from mutatorMath.ufo import build
+    from mutatorMath.ufo.document import DesignSpaceDocumentWriter
+
+    xml_path = 'tmp.designspace'
+    writer = DesignSpaceDocumentWriter(xml_path)
+    family_name = None
+
+    for font in rfonts:
+        save_ufo(font)
+        cur_family_name = font.info.familyName
+        if family_name:
+            assert cur_family_name == family_name
+        else:
+            family_name = cur_family_name
+        writer.addSource(
+            path=font.path,
+            name=font.info.styleName,
+            location={'weight': font.info.weightValue,
+                      'width': font.lib['com.google.glyphs2ufo.widthValue']})
+
+    ofiles = []
+    for instance in instances:
+        style_name = build_style_name(instance, 'weightClass', 'widthClass')
+        ps_name = build_postscript_name(family_name, style_name)
+        cur_path = os.path.join('ufo', ps_name + '.ufo')
+        ofiles.append(cur_path)
+        writer.startInstance(
+            name=instance.pop('name'),
+            location={'weight': instance.pop('interpolationWeight', 100),
+                      'width': instance.pop('interpolationWidth', 100)},
+            familyName=family_name,
+            styleName=style_name,
+            fileName=cur_path,
+            postScriptFontName=ps_name,
+            styleMapFamilyName=family_name,
+            styleMapStyleName=build_style_map_style(style_name))
+        writer.writeInfo()
+        writer.writeKerning()
+        writer.endInstance()
+
+    writer.save()
+    print '>>> Building instances'
+    build(xml_path)
+    for ofile in ofiles:
+        rfont = OpenFont(ofile)
+        rfont.info.postscriptFullName = (
+            build_postscript_name(rfont.info.familyName, rfont.info.styleName))
+        save_ttf(rfont)
+
+    return clear_data(instances)
+
+
 def main(argv):
     #print json.dumps(load(open(sys.argv[1], 'rb')), indent=2, sort_keys=True)
-    rfonts = load_to_rfonts(sys.argv[1])
+    rfonts, instances = load_to_rfonts(sys.argv[1])
     for font in rfonts:
         save_ttf(font)
         #save_ufo(font)
