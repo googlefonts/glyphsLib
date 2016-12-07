1a2d708b45a6c87f56e361a98eb7edbaa399a11b
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-12-07 11:01:11 +0000

    setup.py: bump version to 1.1.0; add PyPI metadata; add install_requires; use pytest runner

--- setup.py
+++ setup.py
@@ -12,19 +12,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+from setuptools import setup, find_packages
 
-from setuptools import setup
 
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='glyphsLib',
-    version='1.0',
-    packages=['glyphsLib'],
+    version='1.1.0',
+    author="James Godfrey-Kittle",
+    author_email="jamesgk@google.com",
+    description="A bridge from Glyphs source files (.glyphs) to UFOs",
+    long_description=long_description,
+    url="https://github.com/googlei18n/glyphsLib",
+    license="Apache Software License 2.0",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
     entry_points={
         "console_scripts": [
             "glyphs2ufo = glyphsLib.__main__:main"
-        ]
+        ],
     },
-    package_dir={'': 'Lib'},
-    test_suite='glyphsLib',
+    setup_requires=pytest_runner,
+    tests_require=[
+        'pytest>=2.8',
+    ],
+    install_requires=[
+        "fonttools>=3.1.2",
+        "defcon>=0.2.0",
+        # TODO(anthrotype): un-comment this out once MutatorMath is on PyPI
+        # "MutatorMath>=2.0.0",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
+    ],
 )
