3d7a434963c38659244983d1f70c83458164f5ac
James Godfrey-Kittle <jamesgk@google.com>
2017-01-12 18:38:33 -0800

    [interpolation] Seperate data extraction, writing
    The master and instance data could also be useful when writing avar
    elements to the designspace document, so move them out of the writing
    functions.

--- Lib/glyphsLib/interpolation.py
+++ Lib/glyphsLib/interpolation.py
@@ -68,25 +68,33 @@ def build_designspace(masters, master_dir, out_dir, instance_data):
     tmp_path = os.path.join(master_dir, 'tmp.designspace')
     writer = DesignSpaceDocumentWriter(tmp_path)
 
-    base_family, base_style = add_masters_to_writer(writer, masters)
-    instance_files = add_instances_to_writer(
-        writer, base_family, instance_data, out_dir)
+    master_data = extract_master_data(masters)
+    base_family = master_data['baseFamily']
+    instance_data = extract_instance_data(instance_data, base_family, out_dir)
 
+    add_masters_to_writer(
+        writer, master_data['masters'], master_data['infoSource'])
+    add_instances_to_writer(writer, instance_data['instances'])
+
+    base_style = master_data['baseStyle']
     basename = '%s%s.designspace' % (
         base_family, ('-' + base_style) if base_style else '')
     writer.path = os.path.join(master_dir, basename.replace(' ', ''))
     writer.save()
-    return writer.path, instance_files
+    return writer.path, instance_data['files']
 
 
-def add_masters_to_writer(writer, ufos):
-    """Add master UFOs to a MutatorMath document writer.
+def extract_master_data(ufos):
+    """Extract interpolation, family and style data from master UFOs.
 
-    Returns the masters' family name and shared style names. These are used for
-    naming instances and the designspace path.
+    Returns data as a dictionary with four keys: "masters" which is a list of
+    (path, family, style, location) tuples for each master, "baseFamily" which
+    is the shared family name between masters, "baseStyle" which is the shared
+    style between masters (could be an empty string), and "infoSource" which is
+    the master from which non-interpolated info should be taken.
     """
 
-    master_data = []
+    master_list = []
     base_family = None
     base_style = None
 
@@ -107,7 +115,7 @@ def add_masters_to_writer(writer, ufos):
             base_style = style.split()
         else:
             base_style = [s for s in style.split() if s in base_style]
-        master_data.append((font.path, family, style, {
+        master_list.append((font.path, family, style, {
             s: font.lib.get(GLYPHS_PREFIX + s + 'Value', DEFAULT_LOC)
             for s in dimension_names}))
 
@@ -116,12 +124,23 @@ def add_masters_to_writer(writer, ufos):
     # defaulting to the first master if nothing is found
     base_style = ' '.join(base_style)
     info_source = 0
-    for i, (path, family, style, location) in enumerate(master_data):
+    for i, (path, family, style, location) in enumerate(master_list):
         if family == base_family and style == (base_style or 'Regular'):
             info_source = i
             break
 
-    for i, (path, family, style, location) in enumerate(master_data):
+    return {
+        'masters': master_list,
+        'baseFamily': base_family,
+        'baseStyle': base_style,
+        'infoSource': info_source,
+    }
+
+
+def add_masters_to_writer(writer, masters, info_source):
+    """Add master UFOs to a MutatorMath document writer."""
+
+    for i, (path, family, style, location) in enumerate(masters):
         is_base = (i == info_source)
         writer.addSource(
             path=path, name='%s %s' % (family, style),
@@ -129,20 +148,18 @@ def add_masters_to_writer(writer, ufos):
             copyFeatures=is_base, copyGroups=is_base, copyInfo=is_base,
             copyLib=is_base)
 
-    return base_family, base_style
 
+def extract_instance_data(instance_data, master_family, out_dir):
+    """Extract interpolation, family and style data from Glyphs instance data.
 
-def add_instances_to_writer(writer, master_family, instance_data, out_dir):
-    """Add instances from Glyphs data to a MutatorMath document writer.
-
-    Returns a list of <ufo_path, font_data> pairs, corresponding to the
-    instances which will be output by the document writer. The font data is the
-    Glyphs data for this instance as a dict.
+    Returns data as a dictionary with two keys: "instances" which is a list of
+    (path, family, style, location) tuples for each instance, and "files" which
+    is a list of (path, instance_data) tuples mapping the path of each instance
+    to its original Glyphs instance data, for use in apply_instance_data.
     """
 
     default_family = instance_data.pop('defaultFamilyName')
     instance_data = instance_data.pop('data')
-    ofiles = []
 
     # only write dimension elements if defined in at least one of the instances
     dimension_names = []
@@ -151,8 +168,9 @@ def add_instances_to_writer(writer, master_family, instance_data, out_dir):
         if any(key in instance for instance in instance_data):
             dimension_names.append(s)
 
+    instance_list = []
+    files = []
     for instance in instance_data:
-
         if not instance.pop('active', True):
             continue
 
@@ -168,23 +186,32 @@ def add_instances_to_writer(writer, master_family, instance_data, out_dir):
             continue
 
         style = instance.pop('name')
-        ufo_path = build_ufo_path(out_dir, family, style)
-        ofiles.append((ufo_path, instance))
+        path = build_ufo_path(out_dir, family, style)
+        instance_list.append((path, family, style, {
+            s: instance.pop('interpolation' + s.title(), DEFAULT_LOC)
+            for s in dimension_names}))
+        files.append((path, instance))
 
+    return {
+        'instances': instance_list,
+        'files': files,
+    }
+
+
+def add_instances_to_writer(writer, instances):
+    """Add instances from Glyphs data to a MutatorMath document writer."""
+
+    for path, family, style, location in instances:
         writer.startInstance(
             name=' '.join((family, style)),
-            location={
-                s: instance.pop('interpolation' + s.title(), DEFAULT_LOC)
-                for s in dimension_names},
+            location=location,
             familyName=family,
             styleName=style,
-            fileName=ufo_path)
+            fileName=path)
 
         writer.writeInfo()
         writer.writeKerning()
         writer.endInstance()
-
-    return ofiles
 
 
 def apply_instance_data(instance_data):
