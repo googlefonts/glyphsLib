39fc8fa68f357550f3f2cee69c865de784d91b9f
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-01-27 00:47:08 +0000

    Release 1.3.0

--- Lib/glyphsLib/__init__.py
+++ Lib/glyphsLib/__init__.py
@@ -28,7 +28,7 @@ from glyphsLib.parser import Parser
 from glyphsLib.util import write_ufo
 
 
-__version__ = "1.3.0.dev0"
+__version__ = "1.3.0"
 
 __all__ = [
     "build_masters", "build_instances", "load_to_ufos", "load", "loads",
