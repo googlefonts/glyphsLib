8d32532e9cfe606f7b365e206ae3d420f5d454d6
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-06 12:05:15 +0000

    Release 1.5.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='glyphsLib',
-    version='1.5.2.dev0',
+    version='1.5.2',
     author="James Godfrey-Kittle",
     author_email="jamesgk@google.com",
     description="A bridge from Glyphs source files (.glyphs) to UFOs",
