6fafb2216f04e554491bc989637fc3d5582bb31b
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-27 12:48:22 +0100

    update minimum required defcon>=0.3 and mutatormath>=2.0.3

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     ],
     install_requires=[
         "fonttools>=3.4.0",
-        "defcon>=0.2.0",
-        "MutatorMath>=2.0.0",
+        "defcon>=0.3.0",
+        "MutatorMath>=2.0.3",
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
