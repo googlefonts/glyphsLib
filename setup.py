bc0b984e2ba13d7d3f125f8e54374432fbf6cf7e
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 22:18:30 +0100

    Bump version: 1.6.1.dev0 → 1.7.0.dev0

--- setup.py
+++ setup.py
@@ -156,7 +156,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='glyphsLib',
-    version='1.6.1.dev0',
+    version='1.7.0.dev0',
     author="James Godfrey-Kittle",
     author_email="jamesgk@google.com",
     description="A bridge from Glyphs source files (.glyphs) to UFOs",
