bee87e4300f5c13a8e11a658b0d2e21a96a147af
Yanone <post@yanone.de>
2017-07-19 14:45:16 +0200

    Implemented GSPath.segments and .bounds

--- Lib/glyphsLib/classes.py
+++ Lib/glyphsLib/classes.py
@@ -21,7 +21,7 @@ import traceback
 import uuid
 import glyphsLib
 from glyphsLib.types import (
-    transform, point, rect, glyphs_datetime, color, floatToString, readIntlist,
+    transform, point, rect, size, glyphs_datetime, color, floatToString, readIntlist,
     writeIntlist, needsQuotes, feature_syntax_encode, baseType
 )
 from glyphsLib.parser import Parser
@@ -1409,14 +1409,58 @@ class GSPath(GSBase):
         lambda self: PathNodesProxy(self),
         lambda self, value: PathNodesProxy(self).setter(value))
 
-    # TODO
     @property
     def segments(self):
-        raise NotImplementedError
+        segments = []
+
+        i = 0
+        while i < len(self.nodes):
+            newSegment = segment()
+            
+            if i == 0:
+                newSegment.append(point(self.nodes[-1].position.x, self.nodes[-1].position.y))
+            else:
+                newSegment.append(point(self.nodes[i-1].position.x, self.nodes[i-1].position.y))
+
+            if self.nodes[i].type == 'offcurve':
+                newSegment.append(point(self.nodes[i].position.x, self.nodes[i].position.y))
+                newSegment.append(point(self.nodes[i + 1].position.x, self.nodes[i + 1].position.y))
+                newSegment.append(point(self.nodes[i + 2].position.x, self.nodes[i + 2].position.y))
+                i += 3
+            elif self.nodes[i].type == 'line':
+                newSegment.append(point(self.nodes[i].position.x, self.nodes[i].position.y))
+                i += 1
+
+            segments.append(newSegment)
+
+        return segments     
 
     @segments.setter
     def segments(self, value):
         raise NotImplementedError
+
+    @property
+    def bounds(self):
+        left, bottom, right, top = None, None, None, None
+        for segment in self.segments:
+            newLeft, newBottom, newRight, newTop = segment.bbox()
+            if left is None:
+                left = newLeft
+            else:
+                left = min(left, newLeft)
+            if bottom is None:
+                bottom = newBottom
+            else:
+                bottom = min(bottom, newBottom)
+            if right is None:
+                right = newRight
+            else:
+                right = max(right, newRight)
+            if top is None:
+                top = newTop
+            else:
+                top = max(top, newTop)
+        return rect(point(left, bottom), point(right - left, top - bottom))
 
     @property
     def direction(self):
@@ -1440,6 +1484,77 @@ class GSPath(GSBase):
 
         # I tried, but it's more complicated than that :(
         self.nodes = reversed(self.nodes)
+
+
+class segment(list):
+    
+    def bbox(self):
+        if len(self) == 2:
+            left = min(self[0].x, self[1].x)
+            bottom = min(self[0].y, self[1].y)
+            right = max(self[0].x, self[1].x)
+            top = max(self[0].y, self[1].y)
+            return left, bottom, right, top
+        elif len(self) == 4:
+            left, bottom, right, top = self.bezierMinMax(self[0].x, self[0].y, self[1].x, self[1].y, self[2].x, self[2].y, self[3].x, self[3].y)
+            return left, bottom, right, top
+        else:
+            raise ValueError
+                
+    def bezierMinMax(self, x0, y0, x1, y1, x2, y2, x3, y3):
+        tvalues = []
+        xvalues = []
+        yvalues = []
+    
+        for i in range(2):
+            if i == 0:
+                b = 6 * x0 - 12 * x1 + 6 * x2
+                a = -3 * x0 + 9 * x1 - 9 * x2 + 3 * x3
+                c = 3 * x1 - 3 * x0
+            else:
+                b = 6 * y0 - 12 * y1 + 6 * y2
+                a = -3 * y0 + 9 * y1 - 9 * y2 + 3 * y3
+                c = 3 * y1 - 3 * y0
+    
+            if abs(a) < 1e-12:
+                if abs(b) < 1e-12:
+                    continue
+                t = -c / b
+                if 0 < t and t < 1:
+                    tvalues.append(t)
+                continue
+    
+            b2ac = b * b - 4 * c * a
+            if b2ac < 0:
+                continue
+            sqrtb2ac = math.sqrt(b2ac)
+            t1 = (-b + sqrtb2ac) / (2 * a)
+            if 0 < t1 and t1 < 1:
+                tvalues.append(t1)
+            t2 = (-b - sqrtb2ac) / (2 * a)
+            if 0 < t2 and t2 < 1:
+                tvalues.append(t2)
+    
+        for j in range(len(tvalues) - 1, -1, -1):
+            t = tvalues[j]
+            mt = 1 - t
+            newxValue = (mt * mt * mt * x0) + (3 * mt * mt * t * x1) + (3 * mt * t * t * x2) + (t * t * t * x3)
+            if len(xvalues) > 0:
+                xvalues[j] = newxValue
+            else:
+                xvalues.append(newxValue)
+            newyValue = (mt * mt * mt * y0) + (3 * mt * mt * t * y1) + (3 * mt * t * t * y2) + (t * t * t * y3)
+            if len(yvalues) > 0:
+                yvalues[j] = newyValue
+            else:
+                yvalues.append(newyValue)
+    
+        xvalues.append(x0)
+        xvalues.append(x3)
+        yvalues.append(y0)
+        yvalues.append(y3)
+    
+        return min(xvalues), min(yvalues), max(xvalues), max(yvalues)
 
 
 class GSComponent(GSBase):
