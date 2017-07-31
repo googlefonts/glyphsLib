2dfc669634ccfa30d717175e009afdd6a753dd2b
Cosimo Lupo <cosimo@anthrotype.com>
2017-07-31 19:26:26 +0100

    Bump version: 1.7.6 → 1.7.7.dev0

--- Lib/glyphsLib/__init__.py
+++ Lib/glyphsLib/__init__.py
@@ -26,7 +26,7 @@ from glyphsLib.parser import Parser
 from glyphsLib.util import write_ufo
 
 
-__version__ = "1.7.6"
+__version__ = "1.7.7.dev0"
 
 __all__ = [
     "build_masters", "build_instances", "load_to_ufos", "load", "loads",
