ad98876b62d7aeea781344d8eeb70a547fe77fd9
Cosimo Lupo <cosimo@anthrotype.com>
2017-08-11 14:01:10 +0100

    [interpolation_test] add tests for set_weight_class and set_width_class

--- tests/interpolation_test.py
+++ tests/interpolation_test.py
@@ -27,7 +27,9 @@ import xml.etree.ElementTree as etree
 import defcon
 from fontTools.misc.py23 import open
 from glyphsLib.builder import GLYPHS_PREFIX
-from glyphsLib.interpolation import build_designspace
+from glyphsLib.interpolation import (
+    build_designspace, set_weight_class, set_width_class,
+)
 
 
 def makeFamily(familyName):
@@ -59,26 +61,34 @@ def makeInstance(name, weight=None, width=None):
     params = []
     if weight is not None:
         # Glyphs 2.3 stores the instance weight in two to three places:
-        # 1. as a textual weightClass (such as “Bold”);
+        # 1. as a textual weightClass (such as “Bold”; no value defaults to
+        #    "Regular");
         # 2. (optional) as numeric customParameters.weightClass (such as 700),
         #    which corresponds to OS/2.usWeightClass where 100 means Thin,
         #    400 means Regular, 700 means Bold, and 900 means Black;
         # 3. as numeric interpolationWeight (such as 66.0), which typically is
-        #    the stem width but can be anything that works for interpolation.
+        #    the stem width but can be anything that works for interpolation
+        #    (no value defaults to 100).
         weightName, weightClass, interpolationWeight = weight
-        result["weightClass"] = weightName
+        if weightName is not None:
+            result["weightClass"] = weightName
         if weightClass is not None:
             params.append({"name": "weightClass", "value": weightClass})
-        result["interpolationWeight"] = interpolationWeight
+        if interpolationWeight is not None:
+            result["interpolationWeight"] = interpolationWeight
     if width is not None:
         # Glyphs 2.3 stores the instance width in two places:
-        # 1. as a textual widthClass (such as “Condensed”);
+        # 1. as a textual widthClass (such as “Condensed”; no value defaults
+        #    to "Medium (normal)");
         # 2. as numeric interpolationWidth (such as 79), which typically is
         #    a percentage of whatever the font designer considers “normal”
-        #    but can be anything that works for interpolation.
+        #    but can be anything that works for interpolation (no value
+        #    defaults to 100).
         widthClass, interpolationWidth = width
-        result["widthClass"] = widthClass
-        result["interpolationWidth"] = interpolationWidth
+        if widthClass is not None:
+            result["widthClass"] = widthClass
+        if interpolationWidth is not None:
+            result["interpolationWidth"] = interpolationWidth
     # TODO: Support custom axes; need to triple-check how these are encoded in
     # Glyphs files. Glyphs 3 will likely overhaul the representation of axes.
     if params:
@@ -277,6 +287,119 @@ class DesignspaceTest(unittest.TestCase):
                              "FamilyName-Italic.designspace")
         finally:
             shutil.rmtree(master_dir)
+
+
+WEIGHT_CLASS_KEY = GLYPHS_PREFIX + "weightClass"
+WIDTH_CLASS_KEY = GLYPHS_PREFIX + "widthClass"
+
+
+class SetWeightWidthClassesTest(unittest.TestCase):
+
+    def test_no_weigth_class(self):
+        ufo = defcon.Font()
+        # name here says "Bold", however no excplit weightClass
+        # is assigned
+        set_weight_class(ufo, makeInstance("Bold"))
+        # the default OS/2 weight class is set
+        self.assertEqual(ufo.info.openTypeOS2WeightClass, 400)
+        # no value is stored in the UFO lib when instance in glyphs
+        # source contains no `weightClass` (because same as default)
+        self.assertTrue(WEIGHT_CLASS_KEY not in ufo.lib)
+
+    def test_weight_class(self):
+        ufo = defcon.Font()
+        data = makeInstance(
+            "Bold",
+            weight=("Bold", None, 150)
+        )
+
+        set_weight_class(ufo, data)
+
+        self.assertEqual(ufo.info.openTypeOS2WeightClass, 700)
+        self.assertEqual(ufo.lib[WEIGHT_CLASS_KEY], "Bold")
+
+    def test_explicit_default_weight(self):
+        ufo = defcon.Font()
+        data = makeInstance(
+            "Regular",
+            weight=("Regular", None, 100)
+        )
+
+        set_weight_class(ufo, data)
+        # the default OS/2 weight class is set
+        self.assertEqual(ufo.info.openTypeOS2WeightClass, 400)
+        # non-empty value is stored in the UFO lib even if same as default
+        self.assertEqual(ufo.lib[WEIGHT_CLASS_KEY], "Regular")
+
+    def test_no_width_class(self):
+        ufo = defcon.Font()
+        # no explicit widthClass set, instance name doesn't matter
+        set_width_class(ufo, makeInstance("Normal"))
+        # the default OS/2 width class is set
+        self.assertEqual(ufo.info.openTypeOS2WidthClass, 5)
+        # no value is stored in the UFO lib when instance in glyphs
+        # source contains no `widthClass` (because same as default)
+        self.assertTrue(WIDTH_CLASS_KEY not in ufo.lib)
+
+    def test_width_class(self):
+        ufo = defcon.Font()
+        data = makeInstance(
+            "Condensed",
+            width=("Condensed", 80)
+        )
+
+        set_width_class(ufo, data)
+
+        self.assertEqual(ufo.info.openTypeOS2WidthClass, 3)
+        self.assertEqual(ufo.lib[WIDTH_CLASS_KEY], "Condensed")
+
+    def test_explicit_default_width(self):
+        ufo = defcon.Font()
+        data = makeInstance(
+            "Regular",
+            width=("Medium (normal)", 100)
+        )
+
+        set_width_class(ufo, data)
+        # the default OS/2 width class is set
+        self.assertEqual(ufo.info.openTypeOS2WidthClass, 5)
+        # non-empty value is stored in the UFO lib even if same as default
+        self.assertEqual(ufo.lib[WIDTH_CLASS_KEY], "Medium (normal)")
+
+    def test_weight_and_width_class(self):
+        ufo = defcon.Font()
+        data = makeInstance(
+            "SemiCondensed ExtraBold",
+            weight=("ExtraBold", None, 160),
+            width=("SemiCondensed", 90)
+        )
+
+        set_weight_class(ufo, data)
+        set_width_class(ufo, data)
+
+        self.assertEqual(ufo.info.openTypeOS2WeightClass, 800)
+        self.assertEqual(ufo.lib[WEIGHT_CLASS_KEY], "ExtraBold")
+        self.assertEqual(ufo.info.openTypeOS2WidthClass, 4)
+        self.assertEqual(ufo.lib[WIDTH_CLASS_KEY], "SemiCondensed")
+
+    def test_unknown_weight_class(self):
+        ufo = defcon.Font()
+        # "DemiLight" is not among the predefined weight classes listed in
+        # Glyphs.app/Contents/Frameworks/GlyphsCore.framework/Versions/A/
+        # Resources/weights.plist
+        # NOTE It is not possible from the user interface to set a custom
+        # string as instance 'weightClass' since the choice is constrained
+        # by a drop-down menu.
+        data = makeInstance(
+            "DemiLight Italic",
+            weight=("DemiLight", 350, 70)
+        )
+
+        set_weight_class(ufo, data)
+
+        # we do not set any OS/2 weight class; user needs to provide
+        # a 'weightClass' custom parameter in this special case
+        self.assertTrue(ufo.info.openTypeOS2WeightClass is None)
 
 
 if __name__ == "__main__":
