01c23c22e88228af0f9d48c9f0f4cdec4b414ce5
Cosimo Lupo <cosimo@anthrotype.com>
2017-09-01 18:17:22 +0100

    [parser_test] add unit tests for Writer class

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -21,7 +21,9 @@ from __future__ import (print_function, division, absolute_import,
 import collections
 import unittest
 
-from glyphsLib.parser import Parser
+from fontTools.misc.py23 import unicode, BytesIO, UnicodeIO
+
+from glyphsLib.parser import Parser, Writer
 
 
 class ParserTest(unittest.TestCase):
@@ -55,6 +57,210 @@ class ParserTest(unittest.TestCase):
         self.run_test(
             b'{mystr="Don\xe2\x80\x99t crash";}',
             [('mystr', 'Don’t crash')])
+
+
+class WriterTest(unittest.TestCase):
+
+    SAMPLE_DATA = collections.OrderedDict([
+        ('a', 'b'),
+        (
+            'c', {
+                'd': 'e'
+            }
+        ),
+        (
+            'f', [
+                'g',
+                'h'
+            ]
+        ),
+        ('i', 'j k l')
+    ])
+
+    def test_text_input_output(self):
+        w = Writer(UnicodeIO())
+        w.write(WriterTest.SAMPLE_DATA)
+        result = w.file.getvalue()
+
+        self.assertIsInstance(result, unicode)
+        self.assertEqual(
+            result.split('\n'),
+            [
+                '{',
+                'a = b;',
+                'c = {',
+                'd = e;',
+                '};',
+                'f = (',
+                'g,',
+                'h',
+                ');',
+                'i = "j k l";',
+                '}',
+                ''
+            ])
+
+    def test_text_input_binary_output(self):
+        w = Writer(BytesIO())
+        w.write(WriterTest.SAMPLE_DATA)
+        result = w.file.getvalue()
+
+        self.assertIsInstance(result, bytes)
+        self.assertEqual(
+            result.split(b'\n'),
+            [
+                b'{',
+                b'a = b;',
+                b'c = {',
+                b'd = e;',
+                b'};',
+                b'f = (',
+                b'g,',
+                b'h',
+                b');',
+                b'i = "j k l";',
+                b'}',
+                b''
+            ])
+
+    def test_binary_input_text_output(self):
+        w = Writer(UnicodeIO())
+        w.write({'name': b'\xc3\xbc'})
+        result = w.file.getvalue()
+        # XXX Glyphs.app writes non-ASCII strings unescaped as UTF-8, whereas
+        # glyphsLib currently escapes all non ASCII characters with \\UXXXX.
+        # self.assertEqual(result, '{\nname = "ü";\n}\n')
+        self.assertEqual(result, '{\nname = "\\U00FC";\n}\n')
+
+    def test_indent_0(self):
+        w = Writer(UnicodeIO(), indent=0)
+        w.write(WriterTest.SAMPLE_DATA)
+
+        self.assertEqual(
+            w.file.getvalue().split('\n'),
+            [
+                '{',
+                'a = b;',
+                'c = {',
+                'd = e;',
+                '};',
+                'f = (',
+                'g,',
+                'h',
+                ');',
+                'i = "j k l";',
+                '}',
+                ''
+            ])
+
+    def test_indent_2(self):
+        w = Writer(UnicodeIO(), indent=2)
+        w.write(WriterTest.SAMPLE_DATA)
+
+        self.assertEqual(
+            w.file.getvalue().split('\n'),
+            [
+                '{',
+                '  a = b;',
+                '  c = {',
+                '    d = e;',
+                '  };',
+                '  f = (',
+                '    g,',
+                '    h',
+                '  );',
+                '  i = "j k l";',
+                '}',
+                ''
+            ])
+
+    def test_indent_4(self):
+        w = Writer(UnicodeIO(), indent=4)
+        w.write(WriterTest.SAMPLE_DATA)
+
+        self.assertEqual(
+            w.file.getvalue().split('\n'),
+            [
+                '{',
+                '    a = b;',
+                '    c = {',
+                '        d = e;',
+                '    };',
+                '    f = (',
+                '        g,',
+                '        h',
+                '    );',
+                '    i = "j k l";',
+                '}',
+                ''
+            ])
+
+    def test_indent_tab(self):
+        w = Writer(UnicodeIO(), indent='\t')
+        w.write(WriterTest.SAMPLE_DATA)
+
+        self.assertEqual(
+            w.file.getvalue().split('\n'),
+            [
+                '{',
+                '\ta = b;',
+                '\tc = {',
+                '\t\td = e;',
+                '\t};',
+                '\tf = (',
+                '\t\tg,',
+                '\t\th',
+                '\t);',
+                '\ti = "j k l";',
+                '}',
+                ''
+            ])
+
+    def test_sort_keys(self):
+        data = dict(WriterTest.SAMPLE_DATA)
+        del data['a']
+        data['b'] = 'a'
+
+        w = Writer(UnicodeIO(), sort_keys=True)
+        w.write(data)
+
+        self.assertEqual(
+            w.file.getvalue().split('\n'),
+            [
+                '{',
+                'b = a;',
+                'c = {',
+                'd = e;',
+                '};',
+                'f = (',
+                'g,',
+                'h',
+                ');',
+                'i = "j k l";',
+                '}',
+                ''
+            ])
+
+    def test_escape_octal(self):
+        w = Writer(UnicodeIO())
+        w.write({'CR': '\u000D'})
+
+        self.assertEqual(w.file.getvalue(), '{\nCR = "\\015";\n}\n')
+
+    def test_escape_inner_quotes(self):
+        w = Writer(UnicodeIO())
+        w.write({'s': 'string with inner "quotes"'})
+
+        self.assertEqual(
+            w.file.getvalue(),
+            '{\ns = "string with inner \\"quotes\\"";\n}\n')
+
+    def test_no_escape(self):
+        s = '"quoted string with escaped inner \\"quotes\\""'
+        w = Writer(UnicodeIO(), escape=False)
+        w.write({'s': s})
+
+        self.assertEqual(w.file.getvalue(), '{\ns = %s;\n}\n' % s)
 
 
 if __name__ == '__main__':
