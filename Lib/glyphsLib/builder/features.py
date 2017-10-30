bca648ef26eebbfb30661f3bc26780c2bf8d027f
Jany Belluz <jany.belluz@daltonmaag.com>
2017-10-30 15:38:31 +0000

    Use `from glyphsLib import glyphdata`

--- Lib/glyphsLib/builder/features.py
+++ Lib/glyphsLib/builder/features.py
@@ -68,7 +68,7 @@ def to_ufo_features(self, ufo):
 
 def _build_gdef(ufo):
     """Build a table GDEF statement for ligature carets."""
-    import glyphsLib.glyphdata  # Expensive import
+    from glyphsLib import glyphdata  # Expensive import
 
     bases, ligatures, marks, carets = set(), set(), set(), {}
     category_key = GLYPHLIB_PREFIX + 'category'
@@ -82,7 +82,7 @@ def _build_gdef(ufo):
             if name and name.startswith('caret_') and 'x' in anchor:
                 carets.setdefault(glyph.name, []).append(round(anchor['x']))
         lib = glyph.lib
-        glyphinfo = glyphsLib.glyphdata.get_glyph(glyph.name)
+        glyphinfo = glyphdata.get_glyph(glyph.name)
         # first check glyph.lib for category/subCategory overrides; else use
         # global values from GlyphData
         category = lib.get(category_key)
