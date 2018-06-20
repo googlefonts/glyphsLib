21404b827e7582d37da31fc2bae477b1e26e85db
Cosimo Lupo <cosimo@anthrotype.com>
2018-06-20 18:15:00 +0100

    Bump version: 2.3.1 → 2.4.0.dev0

--- Lib/glyphsLib/__init__.py
+++ Lib/glyphsLib/__init__.py
@@ -31,7 +31,7 @@ from glyphsLib.parser import load, loads
 from glyphsLib.writer import dump, dumps
 from glyphsLib.util import clean_ufo
 
-__version__ = "2.3.1"
+__version__ = "2.4.0.dev0"
 
 # Doing `import *` from a module that uses unicode_literals, produces
 # "TypeError: Item in ``from list'' must be str, not unicode" on Python 2.
