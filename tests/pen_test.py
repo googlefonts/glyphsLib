2b2b6130c9db7345ae78ed225928c3b3f2cc0bd5
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2020-04-28 17:58:08 +0100

    Test open paths, use point pens

--- tests/pen_test.py
+++ tests/pen_test.py
@@ -2,19 +2,19 @@ import glyphsLib.classes as classes
 
 
 def test_pen_roundtrip(datadir, ufo_module):
-    font = classes.GSFont(str(datadir.join("GlyphsUnitTestSans.glyphs")))
+    font = classes.GSFont(str(datadir.join("PenTest.glyphs")))
     font_temp = ufo_module.Font()
     glyph_temp = font_temp.newGlyph("test")
 
     for glyph in font.glyphs:
         for layer in glyph.layers:
             glyph_temp.clear()
-            ufo_pen = glyph_temp.getPen()
-            layer.draw(ufo_pen)
+            ufo_pen = glyph_temp.getPointPen()
+            layer.drawPoints(ufo_pen)
 
             layer_temp = classes.GSLayer()
-            glyphs_pen = layer_temp.getPen()
-            glyph_temp.draw(glyphs_pen)
+            glyphs_pen = layer_temp.getPointPen()
+            glyph_temp.drawPoints(glyphs_pen)
 
             assert len(layer.paths) == len(layer_temp.paths)
             for path_orig, path_temp in zip(layer.paths, layer_temp.paths):
