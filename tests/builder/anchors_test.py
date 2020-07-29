8a7f27fb09ff2cb3e4345c25330a96408ee43910
Yanone <post@yanone.de>
2020-07-29 17:14:05 +0200

    Testing for new fictional glyph, both source and target positions

--- tests/builder/anchors_test.py
+++ tests/builder/anchors_test.py
@@ -10,6 +10,18 @@ def test_glyphs_font_without_propagated_anchors(datadir):
     assert len(font.glyphs["lam_alefHamzaabove-ar"].layers[0].anchors) == 0
     assert len(font.glyphs["shadda_fatha-ar"].layers[0].anchors) == 0
 
+    # check for `top_1` anchor of lam_alef-ar.short to be in its original location
+    layer = font.glyphs["lam_alef-ar.short"].layers[0]
+    assert "top_1" in [x.name for x in layer.anchors]
+    assert layer.anchors['top_1'].position.x == 498
+    assert layer.anchors['top_1'].position.y == 760
+
+    # check for `top_2` anchor of lam_alef-ar.short to be in its original location
+    layer = font.glyphs["lam_alef-ar.short"].layers[0]
+    assert "top_2" in [x.name for x in layer.anchors]
+    assert layer.anchors['top_2'].position.x == 130
+    assert layer.anchors['top_2'].position.y == 628
+
 
 def test_ufo_with_propagated_anchors(datadir):
 
@@ -28,6 +40,20 @@ def test_ufo_with_propagated_anchors(datadir):
     # lam_alefHamzaabove-ar
     assert "top_2" in [x.name for x in ufo["lam_alefHamzaabove-ar"].anchors]
     for anchor in ufo["lam_alefHamzaabove-ar"].anchors:
+        if anchor.name == "top_2":
+            assert anchor.x == 129
+            assert anchor.y == 950
+
+    # lamHamzaabove_alefHamzaabove-ar (fictional glyph with 2x same component)
+    assert "top_1" in [
+        x.name for x in ufo["lamHamzaabove_alefHamzaabove-ar"].anchors]
+    assert "top_2" in [
+        x.name for x in ufo["lamHamzaabove_alefHamzaabove-ar"].anchors]
+    for anchor in ufo["lamHamzaabove_alefHamzaabove-ar"].anchors:
+        if anchor.name == "top_1":
+            assert anchor.x == 497
+            assert anchor.y == 1082
+    for anchor in ufo["lamHamzaabove_alefHamzaabove-ar"].anchors:
         if anchor.name == "top_2":
             assert anchor.x == 129
             assert anchor.y == 950
