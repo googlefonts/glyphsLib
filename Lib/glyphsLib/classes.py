a75de9d7a69589e0d44aa857141554e5b4d9a26a
Simon Cozens <simon@simon-cozens.org>
2020-10-13 14:27:45 +0100

    Fix out-of-order array access in bezierminmax

--- Lib/glyphsLib/classes.py
+++ Lib/glyphsLib/classes.py
@@ -2073,7 +2073,7 @@ class segment(list):
                 + (3 * mt * t * t * x2)
                 + (t * t * t * x3)
             )
-            if len(xvalues) > 0:
+            if len(xvalues) > j:
                 xvalues[j] = newxValue
             else:
                 xvalues.append(newxValue)
@@ -2083,7 +2083,7 @@ class segment(list):
                 + (3 * mt * t * t * y2)
                 + (t * t * t * y3)
             )
-            if len(yvalues) > 0:
+            if len(yvalues) > j:
                 yvalues[j] = newyValue
             else:
                 yvalues.append(newyValue)
