4caf0e528d7de42a857a7fa439649dc638d7654a
Simon Cozens <simon@simon-cozens.org>
2020-12-14 18:47:15 +0000

    Lock angle is v3 only (??? Check this)

--- Lib/glyphsLib/classes.py
+++ Lib/glyphsLib/classes.py
@@ -1491,6 +1491,11 @@ class GSGuide(GSBase):
     _plistToClass3 = {"pos": "position"}
     _defaultsForName = {"position": Point(0, 0), "angle": 0}
 
+    def shouldWriteValueForKey(self, key, formatVersion):
+        if key == "lockAngle":
+            return formatVersion == 3
+        return super().shouldWriteValueForKey(key, formatVersion)
+
     def position_reader(self, value, formatVersion):
         # This is needed because v3 Points look like lists in the plist
         self["position"] = Point.from_value(value, formatVersion)
