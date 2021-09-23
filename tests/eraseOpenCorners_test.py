699a56e95bfa011ceada3295b7643094ac1daf89
Simon Cozens <simon@simon-cozens.org>
2021-09-23 09:21:05 +0100

    *Failing* test case for “double overlap” glyph

--- tests/eraseOpenCorners_test.py
+++ tests/eraseOpenCorners_test.py
@@ -332,3 +332,20 @@ def test_compatibility2():
     newcontour2 = font2["incompatible2"][0]
     assert len(newcontour1) == len(newcontour2)
     assert structure(newcontour1) == structure(newcontour2)
+
+
+def test_compatibility3():
+    font1 = Font("tests/data/EraseOpenCornersIncompatibilityTest-One.ufo")
+    font2 = Font("tests/data/EraseOpenCornersIncompatibilityTest-Two.ufo")
+    philter = EraseOpenCornersFilter(include={"incompatible3"})
+    import logging
+
+    logger = logging.getLogger("glyphsLib.filters.eraseOpenCorners")
+    logger.setLevel(logging.DEBUG)
+
+    assert philter(font1)
+    newcontour1 = font1["incompatible3"][0]
+    assert philter(font2)
+    newcontour2 = font2["incompatible3"][0]
+    assert len(newcontour1) == len(newcontour2)
+    assert structure(newcontour1) == structure(newcontour2)
