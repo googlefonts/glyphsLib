0b9e1c5c67c0f4bcd27adf34393c9230d79d9770
Simon Cozens <simon@simon-cozens.org>
2021-10-06 11:32:46 +0100

    Support very simple Glyphs 3 bracket layers

--- Lib/glyphsLib/classes.py
+++ Lib/glyphsLib/classes.py
@@ -3712,11 +3712,22 @@ class GSLayer(GSBase):
     BRACKET_LAYER_RE = re.compile(r".*(?P<first_bracket>[\[\]])\s*(?P<value>\d+)\s*\].*")
 
     def _is_bracket_layer(self):
-        return re.match(self.BRACKET_LAYER_RE, self.name)
+        return "axisRules" in self.attr or re.match(self.BRACKET_LAYER_RE, self.name)
 
     def _bracket_info(self):
         if not self._is_bracket_layer():
             return None
+
+        if "axisRules" in self.attr:
+            # Glyphs 3
+            rules = self.attr["axisRules"]
+            if any(rules[1:]):
+                raise ValueError("Alternate rules on non-first axis not yet supported")
+            if "min" in rules[0] and "max" in rules[0]:
+                raise ValueError("Alternate rules with min and max range not yet supported")
+            return 0, rules[0].get("min"), rules[0].get("max")
+
+        # Glyphs 2
         m = re.match(self.BRACKET_LAYER_RE, self.name)
         axis_index = 0 # For glyphs 2
         reverse = m.group("first_bracket") == "]"
