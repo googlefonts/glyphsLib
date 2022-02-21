d9295ad46ab34a3afcd74f185105629059ad9082
Yanone <post@yanone.de>
2022-02-21 18:36:37 +0100

    Added G2 and G3 test files with RTL kerning, making sure both generate identically

--- tests/glyphs3_test.py
+++ tests/glyphs3_test.py
@@ -1,5 +1,6 @@
 import glyphsLib
 from glyphsLib.classes import GSFont, GSFontMaster
+from glyphsLib import to_designspace
 
 
 def test_metrics():
@@ -15,3 +16,15 @@ def test_glyphs3_italic_angle(datadir):
     with open(str(datadir.join("Italic-G3.glyphs"))) as f:
         font = glyphsLib.load(f)
     assert font.masters[0].italicAngle == 11
+
+
+def test_glyphs3_rtl_kerning(datadir, ufo_module):
+    for file in ("RTL_kerning_v2.glyphs", "RTL_kerning_v3.glyphs"):
+        with open(str(datadir.join(file))) as f:
+            font = glyphsLib.load(f)
+
+        designspace = to_designspace(font, ufo_module=ufo_module)
+        ufos = [source.font for source in designspace.sources]
+        assert ufos[0].groups["public.kern2.hah-ar.init"] == ["hah-ar.init"]
+        assert ufos[0].groups["public.kern2.hah-ar.init.swsh"] == ["hah-ar.init.swsh"]
+        assert ufos[0].kerning[("public.kern1.reh-ar", "public.kern2.hah-ar.init.swsh")] == -50
