babc60bafbf0443fb7c3f8ef098ae0d04cef12e3
Simon Cozens <simon@simon-cozens.org>
2022-08-26 09:09:40 +0100

    Tests for glyphspackage loading

--- tests/glyphs3_test.py
+++ tests/glyphs3_test.py
@@ -15,3 +15,10 @@ def test_glyphs3_italic_angle(datadir):
     with open(str(datadir.join("Italic-G3.glyphs"))) as f:
         font = glyphsLib.load(f)
     assert font.masters[0].italicAngle == 11
+
+
+def test_glyphspackage_load(datadir):
+    font1 = glyphsLib.load(str(datadir.join("GlyphsUnitTestSans3.glyphs")))
+    font1.DisplayStrings = "" # glyphspackages, rather sensibly, don't store user state
+    font2 = glyphsLib.load(str(datadir.join("GlyphsUnitTestSans3.glyphspackage")))
+    assert glyphsLib.dumps(font1) == glyphsLib.dumps(font2)
