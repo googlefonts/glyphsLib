8b5190e18bc73c740105ed53ee5787412820bf38
Jens Kutilek <webmail@kutilek.de>
2022-10-07 16:33:59 +0200

    Use isinstance()

--- Lib/glyphsLib/builder/axes.py
+++ Lib/glyphsLib/builder/axes.py
@@ -18,7 +18,7 @@ import logging
 from fontTools.varLib.models import piecewiseLinearMap
 
 from glyphsLib import classes
-from glyphsLib.classes import InstanceType, WEIGHT_CODES, WIDTH_CODES
+from glyphsLib.classes import GSInstance, InstanceType, WEIGHT_CODES, WIDTH_CODES
 from glyphsLib.builder.constants import WIDTH_CLASS_TO_VALUE
 
 logger = logging.getLogger(__name__)
@@ -414,7 +414,7 @@ class AxisDefinition:
 
     def set_user_loc(self, master_or_instance, value):
         """Set the user location of a Glyphs master or instance."""
-        if hasattr(master_or_instance, "instanceInterpolations"):
+        if isinstance(master_or_instance, GSInstance):
             # The following code is only valid for instances.
             # Masters also the keys `weight` and `width` but they should not be
             # used, they are deprecated and should only be used to store
