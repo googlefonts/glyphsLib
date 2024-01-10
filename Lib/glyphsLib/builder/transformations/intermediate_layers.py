59211297acf3ff5bcc82c2141dc1b2ab291eeb76
Simon Cozens <simon@simon-cozens.org>
2024-01-10 13:59:38 +0000

    Add the "intermediate layers" transformation

---
+++ Lib/glyphsLib/builder/transformations/intermediate_layers.py
@@ -0,0 +1,135 @@
+import logging
+import uuid
+
+from fontTools.varLib.models import VariationModel, normalizeValue
+
+from glyphsLib.classes import GSLayer, GSNode, GSPath
+
+
+logger = logging.getLogger(__name__)
+
+
+def resolve_intermediate_components(font):
+    components_with_intermediate_layers = set()
+    all_intermediate_locations = set()
+    for glyph in font.glyphs:
+        for layer in glyph.layers:
+            if "coordinates" in layer.attributes:
+                # First, let's find glyphs with intermediate layers
+                # which have components which don't have intermediate layers
+                for shape in layer.components:
+                    check_component_has_sparse_layer(font, shape, layer)
+                # Later we will check if everyone who uses me as a component
+                # has the same intermediate layers as I do
+                components_with_intermediate_layers.add(glyph.name)
+                all_intermediate_locations.add(tuple(layer.attributes["coordinates"]))
+
+    # It's not clear to me that this is needed... (See #954)
+
+    # for glyph in font.glyphs:
+    #     # Just peek at first layer, assume compatibility
+    #     if not any(c.name in components_with_intermediate_layers for c in glyph.layers[0].components):
+    #         continue
+    #     for location in all_intermediate_locations:
+    #         if not any(list(location) == l.attributes.get("coordinates") for l in glyph.layers):
+    #             import IPython; IPython.embed()
+    #             logger.info("Adding intermediate layer at %s to %s", location, glyph.name)
+    #             layer = GSLayer()
+    #             layer.attributes["coordinates"] = location
+    #             layer.layerId = str(uuid.uuid4())
+    #             layer.associatedMasterId = glyph.layers[0].associatedMasterId
+    #             layer.name = glyph.layers[0].name
+    #             for ix, shape in enumerate(glyph.layers[0].shapes):
+    #                 if isinstance(shape, GSPath):
+    #                     layer.shapes.append(shape.clone())
+    #                 else:
+    #                     layer.shapes.append(shape.clone())
+    #             glyph.layers.append(layer)
+
+
+def simple_variation_model(font):
+    tags = [axis.axisTag for axis in font.axes]
+    locations = [x.axes for x in font.masters]
+    limits = {tag: (min(x), max(x)) for tag, x in zip(tags, (zip(*locations)))}
+    master_locations = []
+    for loc in locations:
+        this_loc = {}
+        for ix, axisTag in enumerate(tags):
+            axismin, axismax = limits[axisTag]
+            this_loc[axisTag] = normalizeValue(loc[ix], (axismin, axismin, axismax))
+        master_locations.append(this_loc)
+    return VariationModel(master_locations, axisOrder=tags), limits
+
+
+def check_component_has_sparse_layer(font, component, parent_layer):
+    tags = [axis.axisTag for axis in font.axes]
+    model, limits = simple_variation_model(font)
+    location = parent_layer.attributes["coordinates"]
+    normalized_location = {
+        axisTag: normalizeValue(
+            location[ix], (limits[axisTag][0], limits[axisTag][0], limits[axisTag][1])
+        )
+        for ix, axisTag in enumerate(tags)
+    }
+    componentglyph = component.component
+    for layer in componentglyph.layers:
+        if layer.layerId == parent_layer.layerId:
+            return
+        if (
+            "coordinates" in layer.attributes
+            and layer.attributes["coordinates"] == location
+        ):
+            return
+
+    # We'll add the appropriate intermediate layer to the component, that'll fix it
+    logger.info(
+        "Adding intermediate layer to %s to support %s %s",
+        componentglyph.name,
+        parent_layer.parent.name,
+        parent_layer.name,
+    )
+    layer = GSLayer()
+    layer.attributes["coordinates"] = location
+    layer.layerId = str(uuid.uuid4())
+    layer.associatedMasterId = parent_layer.associatedMasterId
+    layer.name = parent_layer.name
+    for ix, shape in enumerate(componentglyph.layers[0].shapes):
+        all_shapes = [l.shapes[ix] for l in componentglyph.layers if l._is_master_layer]
+        assert len(all_shapes) == len(font.masters)
+        if isinstance(shape, GSPath):
+            # We are making big assumptions about compatibility here
+            layer.shapes.append(
+                interpolate_path(all_shapes, model, normalized_location)
+            )
+        else:
+            # We should really interpolate the transformation matrix here
+            check_component_has_sparse_layer(font, shape, parent_layer)
+            layer.shapes.append(
+                interpolate_component(all_shapes, model, normalized_location)
+            )
+    componentglyph.layers.append(layer)
+
+
+def interpolate_path(paths, model, location):
+    path = GSPath()
+    for master_nodes in zip(*[p.nodes for p in paths]):
+        node = GSNode()
+        node.type = master_nodes[0].type
+        node.smooth = master_nodes[0].smooth
+        xs = [n.position.x for n in master_nodes]
+        ys = [n.position.y for n in master_nodes]
+        node.position.x = model.interpolateFromMasters(location, xs)
+        node.position.y = model.interpolateFromMasters(location, ys)
+        path.nodes.append(node)
+    return path
+
+
+def interpolate_component(components, model, location):
+    component = components[0].clone()
+    if all(c.transform == component.transform for c in components):
+        return component
+    transforms = [c.transform for c in components]
+    for element in range(6):
+        values = [t[element] for t in transforms]
+        component.transform[element] = model.interpolateFromMasters(location, values)
+    return component
