baf3139d441ed028dd3bcdc2aaeab3d01b8b6fec
Cosimo Lupo <clupo@google.com>
2026-02-26 09:49:18 +0000

    Narrow _contains exception handler to TypeError

--- Lib/glyphsLib/builder/tokens.py
+++ Lib/glyphsLib/builder/tokens.py
@@ -11,7 +11,7 @@ def _like(got, expected):
 def _contains(got, expected):
     try:
         return expected in got
-    except Exception:
+    except TypeError:
         return expected in str(got)
 
 
